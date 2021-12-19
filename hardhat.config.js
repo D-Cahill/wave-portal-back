f52775fd3ffcc77f949692bb7371f78febc6e35d
Darragh Cahill <darragh.cahill@protonmail.com>
2021-12-18 23:54:17 -0800

    Initial Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/oCeKBYTGtElFJ9OqXvF3f5GAWhC88UNP',
+      url: 'STAGING_ALCHEMY_KEY',
       accounts: ['3eb6ce6a746bf44dad27164520d91f59714d89b3e12093c6f46567543ec862ad'],
     },
   },
